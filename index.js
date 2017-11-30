7801ebdef4f552c2ea50d4f52da7cdefe352e191
Angela Ahn <29741357+aahn84@users.noreply.github.com>
2017-11-30 09:47:05 -0800

    complete express warmup

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let express = require('express')
+//similar to <script src="express"></script>
 let app = express()
 
 let dogs = ['bowser', 'woof', 'dog3']
 let cats = ['meow', 'lexington', 'cat3']
 
 // add two new arrays of your choice
+let burgers = ['beef', 'chicken', 'fish', 'mushroom']
+let pizzas = ['combo', 'pepperoni', 'cheese']
 
 app.get('/dogs', function(req, res) {
   res.json(dogs)
@@ -15,6 +18,13 @@ app.get('/cats', function(req, res) {
 })
 
 // add two new routes of your choice
+app.get('/burgers', function(req, res) {
+  res.json(burgers)
+})
+
+app.get('/pizzas', function(req, res) {
+  res.json(pizzas)
+})
 
 app.listen(3000, function() {
   console.log('listening on port 3000')
